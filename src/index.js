6c41d782184eb7708ef48d2ebef29884aa4cbd34
Dmitry Khasanov <dkhasanov@avito.ru>
2016-06-25 23:11:52 +0300

    Button added to send location.

--- src/index.js
+++ src/index.js
@@ -3,26 +3,30 @@
 var React = require('react');
 var ReactNative = require('react-native');
 var {
+    AppRegistry,
+    StyleSheet,
     View,
     Text,
-    StyleSheet,
-    AppRegistry,
 } = ReactNative;
 var MapView = require('react-native-maps');
+var Btn = require('Btn');
 
 var Moov = React.createClass({
     render: function() {
         return (
             <View style={styles.container}>
-                <MapView
-                    style={styles.map}
-                    initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
-                />
+                <View style={styles.mapContainer}>
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            latitude: 55.774332,
+                            longitude: 37.604595,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                    />
+                </View>
+                <Btn/>
             </View>
         );
     }
@@ -33,10 +37,14 @@ var styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
     },
+    mapContainer: {
+        flex: 1,
+        flexDirection: 'column',
+    },
     map: {
         flex: 1,
         flexDirection: 'column',
-    }
+    },
 });
 
 AppRegistry.registerComponent('Moov', () => Moov);
